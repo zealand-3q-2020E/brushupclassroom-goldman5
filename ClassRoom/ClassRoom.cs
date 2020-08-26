e2eb5901b3f90cacc168a46358a08096c50deee6
camachocv@hotmail.com <elvi0506@edu.easj.dk>
2020-08-26 12:51:13 +0200

    task 2

---
+++ ClassRoom/ClassRoom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class ClassRoom
+    {
+        public string ClassName { get; set; }
+
+        public List<Student> classList { get; set; }
+        public DateTime SemesterStart { get; set; }
+
+        public ClassRoom()
+        {
+            
+        }
+    }
+}
