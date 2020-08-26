bab87482fe60cd94bf0e24e9d3fa0991013e48ec
camachocv@hotmail.com <elvi0506@edu.easj.dk>
2020-08-26 12:43:16 +0200

    Task 1

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Student
+    {
+        public string Name { get;  set; }
+        public int BirthMonth { get; set; }
+        public int Birthday { get; set; }
+
+        public Student(string name, int birthMonth, int birthday)
+        {
+            name = Name;
+            birthMonth = BirthMonth;
+            birthday = Birthday;
+        }
+
+
+    }
+}
